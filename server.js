d21da2d1892224547c17f8df87b48375b4961665
Deniz3045 <denizgoalie45@gmail.com>
2026-02-27 21:33:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -115,4 +115,4 @@ io.on('connection', (socket)=>{
 
 });
 
-server.listen(3000,()=>console.log('Server läuft auf http://localhost:3000'));
+server.listen(3000,()=>console.log('Server läuft auf spquiz.onrender.com/index.html'));
